a62822706522116fb4290228370a887d41195a81
Alex Montague <xturnermonty@gmail.com>
2018-10-20 18:36:08 -0400

    Updated minor errors, added picture updates, tried to convert gifs to mp4 did not work

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
 
 class Portfolio extends Component {
+  renderMedia = file => {
+    const filePath = `images/portfolio/${file}`;
+    // videos are messed up
+    // if (file.includes(".mp4")) {
+    //   // if a video
+    //   return (
+    //     <video
+    //       autoplay
+    //       loop
+    //       muted
+    //       playsinline
+    //       className="portfolioImage"
+    //       src={filePath}
+    //     />
+    //   );
+    // }
+
+    return (
+      <img src={filePath} alt="portfolio item" className="portfolioImage" />
+    );
+  };
+
   render() {
-    const projects = this.props.data.projects.map((project, index) => {
+    const projects = this.props.data.projects.map(project => {
       const { title, category, url, image } = project;
-      const projectImage = "images/portfolio/" + image;
       return (
         <div key={title}>
           <a href={url} target="_blank">
             <div className="portfolioItem">
               <h5>{title}</h5>
               <span className="flexMeUp">
-                <img
-                  src={projectImage}
-                  alt="portfolio item"
-                  className="portfolioImage"
-                />
+                {this.renderMedia(image)}
                 {category}
               </span>
             </div>
