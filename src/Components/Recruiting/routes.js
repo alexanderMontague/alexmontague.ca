5dd2ee941691b43f0dc1fd12ff4507118df765e6
alexanderMontague <xturnermonty@gmail.com>
2019-07-28 20:43:14 -0400

    add big ticket recruiting pages

--- src/Components/Recruiting/routes.js
+++ src/Components/Recruiting/routes.js
@@ -118,6 +118,146 @@ const recruitingRoutes = [
     companyName="Okta"
     companyColors={["#337CBB", "#5CABDF"]}
     textColor="#ffffff"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/easypost"
+    companyName="Easy Post"
+    companyColors={["#324199", "#324199"]}
+    textColor="#ffffff"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/udemy"
+    companyName="Udemy"
+    companyColors={["#DB5D58", "#DB5D58"]}
+    textColor="#ffffff"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/amazon"
+    companyName="Amazon"
+    companyColors={["#ECA63F", "#ECA63F"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/airbnb"
+    companyName="Airbnb"
+    companyColors={["#ED6664", "#ED6664"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/cohesity"
+    companyName="Cohesity"
+    companyColors={["#9FC756", "#9FC756"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/github"
+    companyName="GitHub"
+    companyColors={["#171515", "#171515"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/salesforce"
+    companyName="Salesforce"
+    companyColors={["#479BD2", "#479BD2"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/roku"
+    companyName="Roku"
+    companyColors={["#4B327D", "#4B327D"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/linkedin"
+    companyName="LinkedIn"
+    companyColors={["#3D82B7", "#3D82B7"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/youtube"
+    companyName="YouTube"
+    companyColors={["#EB3232", "#EB3232"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/evernote"
+    companyName="Evernote"
+    companyColors={["#4BA534", "#4BA534"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/rakuten"
+    companyName="Rakuten"
+    companyColors={["#AE2317", "#AE2317"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/mongodb"
+    companyName="MongoDB"
+    companyColors={["#64A757", "#64A757"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/stripe"
+    companyName="Stripe"
+    companyColors={["#5F5AC8", "#5F5Ac8"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/square"
+    companyName="Square"
+    companyColors={["#171515", "#171515"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/slack"
+    companyName="Slack"
+    companyColors={["#63C3EC", "#E3B34B"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/twitter"
+    companyName="Twitter"
+    companyColors={["#67AAEB", "#67AAEB"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/yelp"
+    companyName="Yelp"
+    companyColors={["#AE2317", "#AE2317"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/intuit"
+    companyName="Intuit"
+    companyColors={["#3777B8", "#6DB9E6"]}
+    textColor="#FFFFFF"
+  />,
+
+  <DefaultRecruitingComponent
+    path="/instacart"
+    companyName="Instacart"
+    companyColors={["#F08933", "#F08933"]}
+    textColor="#FFFFFF"
   />
 ];
 
