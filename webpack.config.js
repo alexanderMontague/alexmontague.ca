560ee6e2711cebbbcc6bb8e7e9d2d70e96e5c669
Alex Montague <xturnermonty@gmail.com>
2020-01-16 21:35:43 -0500

    HUGE website refactor, switch to webpack and update everything

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = env => {
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.join(__dirname, "/build"),
+      filename: "index_bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(jpg|png)$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 25000
+            }
+          }
+        }
+      ]
+    },
+    devServer: {
+      open: true,
+      hot: true,
+      port: process.env.PORT || "3010",
+      publicPath: "/",
+      contentBase: "public",
+      watchContentBase: true,
+      historyApiFallback: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html"
+      }),
+      new webpack.DefinePlugin({})
+    ]
+  };
+};
