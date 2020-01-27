db0dfa8296c3958cb882ae02e2e4b18fe80be8cb
alexanderMontague <xturnermonty@gmail.com>
2020-01-26 20:18:43 -0500

    add webpack cleanup on builds

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = env => {
   return {
@@ -48,8 +49,9 @@ module.exports = env => {
       new HtmlWebpackPlugin({
         template: "src/index.html"
       }),
-      new CopyPlugin([{ from: "public", to: "public" }]),
-      new webpack.DefinePlugin({})
+      new CopyPlugin([{ from: "public", to: "public" }]), // copies static files to build folder
+      new webpack.DefinePlugin({}),
+      new CleanWebpackPlugin()
     ]
   };
 };
